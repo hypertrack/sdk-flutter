96f1b6c2b63c38adcd591a45c5f36d330768bdb5
Denys <quux@ukr.net>
2021-04-06 17:47:59 +0300

    Docstrings were added

--- lib/hypertrack.dart
+++ lib/hypertrack.dart
@@ -3,19 +3,30 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 enum TrackingStateChange {
+  /// Tracking is active
   start,
+  /// Tracking stopped
   stop,
+  /// Tracking doesn't happen due to missing permission(s)
   permissions_denied,
+  /// Tracking doesn't happend due to location service being disabled.
   location_disabled,
   invalid_token,
+  /// SDK encountered network error
   network_error,
+  /// Tracking won't happen as either account is suspended or device was deleted
+  /// from account.
   auth_error,
   unknown_error
 }
 
-
+/// Plugin allows you to use your application as a location data source
+/// feeding HyperTrack platform.
 class HyperTrack {
 
+  /// Use this method to get the SDK instance.
+  ///
+  /// SDK will connect to the account identified by [publishableKey].
   static Future<HyperTrack> initialize(publishableKey) async {
     final MethodChannel methodChannel =
     const MethodChannel('sdk.hypertrack.com/handle');
@@ -25,6 +36,7 @@ class HyperTrack {
     return HyperTrack(methodChannel, eventChannel);
   }
 
+  /// Enable internal SDK logging.
   static void enableDebugLogging() {
     const MethodChannel('sdk.hypertrack.com/handle')
         .invokeMethod('enableDebugLogging');
@@ -36,24 +48,66 @@ class HyperTrack {
 
   HyperTrack(this._methodChannel, this._eventChannel);
 
+  /// Returns string that uniquely identifies device in HyperTrack platform.
   Future<String> getDeviceId() async => await _methodChannel.invokeMethod<String>('getDeviceId');
 
+  /// Returns `true` if tracking was started.
+  ///
+  /// This doesn't actually means that SDK collecting location data, but only
+  /// allows you to check the current state of tracking state switch.
+  /// For details on whether tracking actually happens or not check [onTrackingStateChanged] events.
   Future<bool> isRunning() async => await _methodChannel.invokeMethod<bool>('isRunning');
 
+  /// Triggers tracking start.
+  ///
+  /// This isn't always result in SDK tracking, as missing permissions or disabled
+  /// geolocation sensors could lead to a tracking outage. Use [onTrackingStateChanged]
+  /// stream to get the actual state details.
   void start()  => _methodChannel.invokeMethod<void>('start');
 
+  /// Stops tracking.
   void stop() =>  _methodChannel.invokeMethod<void>('stop');
 
+  /// Adds geotag with configurable payload.
+  ///
+  /// Please, bear in mind that this will be serialized as json so passing in
+  /// recursive data structure could lead to unpredictable results.
   void addGeotag(Map<String, Object> data) => _methodChannel.invokeMethod('addGeotag', data);
 
+  /// Sets current device name, that can be used for easier dashboard navigation.
+  ///
+  /// This setter is intended to be used for static data, that doesn't change
+  /// during tracking sessions, as no SLA regarding this data propagation
+  /// provided. In case of frequent changes, it is possible, that intermediate
+  /// states could be lost, so if more real-time responsiveness is required
+  /// it is recommended to use [addGeotag] for passing that data.
   void setDeviceName(String name) => _methodChannel.invokeMethod('setDeviceName', name);
 
+  /// Sets current device data.
+  ///
+  /// The data can be used for easier dashboard navigation or grouping of
+  /// location data in tag groups style.
+  /// This setter is intended to be used for static data, that doesn't change
+  /// during tracking sessions, as no SLA regarding this data propagation provided.
+  /// In case of frequent changes, it is possible, that intermediate states
+  /// could be lost, so if more real-time responsiveness is required it is
+  /// recommended to use [addGeotag] for passing that data.
   void setDeviceMetadata(Map<String, Object> data) => _methodChannel.invokeMethod('setDeviceMetadata', data);
 
+  /// This method checks with HyperTrack cloud whether to start or stop tracking.
+  ///
+  /// Tracking starts when Devices or Trips API is used to either to start
+  /// the device tracking or when a trip is created for this device.
   void syncDeviceSettings() => _methodChannel.invokeMethod('syncDeviceSettings');
 
+  /// Allows you to use location mocking software (e.g. for development).
+  ///
+  /// Mock locations are ignored by HyperTrack SDK by default.
   void allowMockLocations() => _methodChannel.invokeMethod('allowMockLocations');
 
+  /// Realtime updates of SDK state.
+  ///
+  /// Check [TrackingStateChange] for an explanation.
   Stream<TrackingStateChange> get onTrackingStateChanged {
     if (_trackingStateStream == null) {
       _trackingStateStream = _eventChannel
