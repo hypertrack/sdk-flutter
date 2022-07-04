c885eb4538828c08cd73287c6c49fb8e52937d88
pavel-kuznetsov-hypertrack <78730314+pavel-kuznetsov-hypertrack@users.noreply.github.com>
2022-07-04 20:07:56 +0300

    Add support for null safety. Update plugin structure. Add addGeotag, enableDebugLogging methods. Fix nullability issues. Add better logging.

--- example/android/app/src/main/kotlin/com/hypertrack/sdk/hypertrack_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/hypertrack/sdk/hypertrack_example/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.hypertrack.sdk.hypertrack_example
 
-import androidx.annotation.NonNull;
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
+import android.os.Bundle
+import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(this)
     }
 }
