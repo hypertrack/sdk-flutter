cad3a26da9938adfd8352cb7f1a034489219d00e
pavel-kuznetsov-hypertrack <pavel@hypertrack.io>
2024-01-08 15:59:30 +0200

    Add method comments

--- lib/hypertrack.dart
+++ lib/hypertrack.dart
@@ -1,5 +1,470 @@
 import 'dart:ffi';
 import 'dart:developer';
+/*
+
+export default class HyperTrack {
+private static locateSubscription: EmitterSubscription | undefined;
+
+/**
+ * Adds a new geotag
+ *
+ * @param {Object} data - Geotad data JSON
+ * @returns current location if success or LocationError if failure
+ */
+static async addGeotag(
+data: Object
+): Promise<Result<Location, LocationError>>;
+
+/**
+ * Adds a new geotag with expected location
+ *
+ * @param {Object} data - Geotad data JSON
+ * @param {Location} expectedLocation - Expected location
+ * @returns location with deviation if success or LocationError if failure
+ */
+static async addGeotag(
+data: Object,
+expectedLocation: Location
+): Promise<Result<LocationWithDeviation, LocationError>>;
+
+static async addGeotag(
+...args: any[]
+): Promise<Result<Location | LocationWithDeviation, LocationError>> {
+if (args.length === 1 && typeof args[0] === 'object') {
+return HyperTrackSdk.addGeotag({
+data: args[0],
+expectedLocation: undefined,
+} as GeotagData).then(
+(locationResponse: Result<LocationInternal, LocationErrorInternal>) => {
+return this.deserializeLocationResponse(locationResponse);
+}
+);
+} else if (
+args.length === 2 &&
+typeof args[0] === 'object' &&
+HyperTrack.isLocation(args[1])
+) {
+let expectedLocation = args[1] as Location;
+return HyperTrackSdk.addGeotag({
+data: args[0],
+expectedLocation: {
+type: 'location',
+value: {
+latitude: expectedLocation.latitude,
+longitude: expectedLocation.longitude,
+},
+} as LocationInternal,
+} as GeotagData).then(
+(
+locationResponse: Result<
+LocationWithDeviationInternal,
+LocationErrorInternal
+>
+) => {
+return this.deserializeLocationWithDeviationResponse(
+locationResponse
+);
+}
+);
+} else {
+throw new Error('Invalid arguments');
+}
+}
+
+/**
+ * Returns a string that is used to uniquely identify the device
+ *
+ * @returns {string} Device ID
+ */
+static async getDeviceId(): Promise<string> {
+return HyperTrackSdk.getDeviceId().then(
+(deviceId: DeviceId) => deviceId.value
+);
+}
+
+/**
+ * Returns a list of errors that blocks SDK from tracking
+ *
+ * @returns {HyperTrackError[]} List of errors
+ */
+static async getErrors(): Promise<HyperTrackError[]> {
+return HyperTrackSdk.getErrors().then(
+(errors: HyperTrackErrorInternal[]) => {
+return this.deserializeHyperTrackErrors(errors);
+}
+);
+}
+
+/**
+ * Reflects availability of the device for the Nearby search
+ *
+ * @returns true when is available or false when unavailable
+ */
+static async getIsAvailable(): Promise<boolean> {
+return HyperTrackSdk.getIsAvailable().then(
+(isAvailable: IsAvailable) => isAvailable.value
+);
+}
+
+/**
+ * Reflects the tracking intent for the device
+ *
+ * @returns {boolean} Whether the user's movement data is getting tracked or not.
+ */
+static async getIsTracking(): Promise<boolean> {
+return HyperTrackSdk.getIsTracking().then(
+(isTracking: IsTracking) => isTracking.value
+);
+}
+
+/**
+ * Reflects the current location of the user or an outage reason
+ */
+static async getLocation(): Promise<Result<Location, LocationError>> {
+return HyperTrackSdk.getLocation().then(
+(locationResponse: Result<LocationInternal, LocationErrorInternal>) => {
+return this.deserializeLocationResponse(locationResponse);
+}
+);
+}
+
+/**
+ * Gets the metadata that is set for the device
+ *
+ * @returns {Object} Metadata JSON
+ */
+static async getMetadata(): Promise<Object> {
+return HyperTrackSdk.getMetadata().then((metadata: Metadata) => {
+return this.deserializeMetadata(metadata);
+});
+}
+
+/**
+ * Gets the name that is set for the device
+ *
+ * @returns {string} Device name
+ */
+static async getName(): Promise<string> {
+return HyperTrackSdk.getName().then((name: Name) => {
+return this.deserializeName(name);
+});
+}
+
+/**
+ * Requests one-time location update and returns the location once it is available, or error.
+ *
+ * Only one locate subscription can be active at a time. If you re-subscribe, the old EmitterSubscription
+ * will be automaticaly removed.
+ *
+ * This method will start location tracking if called, and will stop it when the location is received or
+ * the subscription is cancelled. If any other tracking intent is present (e.g. isAvailable is set to `true`),
+ * the tracking will not be stopped.
+ *
+ * @param callback
+ * @returns EmitterSubscription
+ * @example
+ * ```js
+ * const subscription = HyperTrack.locate(location => {
+ *  ...
+ * })
+ *
+ * // to unsubscribe
+ * subscription.remove()
+ * ```
+ */
+static locate(
+callback: (location: Result<Location, HyperTrackError[]>) => void
+) {
+this.locateSubscription?.remove();
+this.locateSubscription = EventEmitter.addListener(
+EVENT_LOCATE,
+(location: Result<LocationInternal, HyperTrackErrorInternal[]>) => {
+callback(this.deserializeLocateResponse(location));
+}
+);
+HyperTrackSdk.locate();
+return this.locateSubscription;
+}
+
+/**
+ * Sets the availability of the device for the Nearby search
+ *
+ * @param availability true when is available or false when unavailable
+ */
+static async setIsAvailable(isAvailable: boolean) {
+HyperTrackSdk.setIsAvailable({
+type: 'isAvailable',
+value: isAvailable,
+} as IsAvailable);
+}
+
+/**
+ * Sets the tracking intent for the device
+ *
+ * @param {boolean} isTracking
+ */
+static async setIsTracking(isTracking: boolean): Promise<void> {
+HyperTrackSdk.setIsTracking({
+type: 'isTracking',
+value: isTracking,
+} as IsTracking);
+}
+
+/**
+ * Sets the metadata for the device
+ *
+ * @param {Object} data - Metadata JSON
+ */
+static async setMetadata(data: Object) {
+HyperTrackSdk.setMetadata({
+type: 'metadata',
+value: data,
+});
+}
+
+/**
+ * Sets the name for the device
+ *
+ * @param {string} name
+ */
+static async setName(name: string) {
+HyperTrackSdk.setName({
+type: 'name',
+value: name,
+} as Name);
+}
+
+/**
+ * Subscribe to tracking errors
+ *
+ * @param listener
+ * @returns EmitterSubscription
+ * @example
+ * ```js
+ * const subscription = HyperTrack.subscribeToErrors(errors => {
+ *   errors.forEach(error => {
+ *     // ... error
+ *   })
+ * })
+ *
+ * // later, to stop listening
+ * subscription.remove()
+ * ```
+ */
+static subscribeToErrors(
+listener: (errors: HyperTrackError[]) => void
+): EmitterSubscription {
+return EventEmitter.addListener(
+EVENT_ERRORS,
+(rawErrors: HyperTrackErrorInternal[]) => {
+listener(this.deserializeHyperTrackErrors(rawErrors));
+}
+);
+}
+
+/**
+ * Subscribe to availability changes
+ *
+ * @param listener
+ * @returns EmitterSubscription
+ * @example
+ * ```js
+ * const subscription = HyperTrack.subscribeToIsAvailable(isAvailable => {
+ *   if (isAvailable) {
+ *     // ... ready to go
+ *   }
+ * })
+ *
+ * // later, to stop listening
+ * subscription.remove()
+ * ```
+ */
+static subscribeToIsAvailable(
+listener: (isAvailable: boolean) => void
+): EmitterSubscription {
+return EventEmitter.addListener(
+EVENT_IS_AVAILABLE,
+(isAvailable: IsAvailable) => {
+listener(isAvailable.value);
+}
+);
+}
+
+/**
+ * Subscribe to tracking intent changes
+ *
+ * @param listener
+ * @returns EmitterSubscription
+ * @example
+ * ```js
+ * const subscription = HyperTrack.subscribeToIsTracking(isTracking => {
+ *   if (isTracking) {
+ *     // ... ready to go
+ *   }
+ * })
+ *
+ * // later, to stop listening
+ * subscription.remove()
+ * ```
+ */
+static subscribeToIsTracking(
+listener: (isTracking: boolean) => void
+): EmitterSubscription {
+return EventEmitter.addListener(
+EVENT_IS_TRACKING,
+(isTracking: IsTracking) => {
+listener(isTracking.value);
+}
+);
+}
+
+/**
+ * Subscribe to location changes
+ *
+ * @param listener
+ * @returns EmitterSubscription
+ * @example
+ * ```js
+ * const subscription = HyperTrack.subscribeToLocation(location => {
+ *   ...
+ * })
+ *
+ * // later, to stop listening
+ * subscription.remove()
+ * ```
+ */
+static subscribeToLocation(
+listener: (location: Result<Location, LocationError>) => void
+) {
+return EventEmitter.addListener(
+EVENT_LOCATION,
+(location: Result<LocationInternal, LocationErrorInternal>) => {
+listener(this.deserializeLocationResponse(location));
+}
+);
+}
+
+/** @ignore */
+private static deserializeHyperTrackErrors(
+errors: HyperTrackErrorInternal[]
+): HyperTrackError[] {
+let res = errors.map((error: HyperTrackErrorInternal) => {
+if (error.type != 'error') {
+throw new Error('Invalid error type');
+}
+return Object.keys(HyperTrackError).find(
+(key) =>
+HyperTrackError[key as keyof typeof HyperTrackError] === error.value
+) as HyperTrackError;
+});
+return res;
+}
+
+/** @ignore */
+private static deserializeLocateResponse(
+response: Result<LocationInternal, HyperTrackErrorInternal[]>
+): Result<Location, HyperTrackError[]> {
+switch (response.type) {
+case 'success':
+return {
+type: 'success',
+value: response.value.value,
+};
+case 'failure':
+return {
+type: 'failure',
+value: this.deserializeHyperTrackErrors(response.value),
+};
+}
+}
+
+/** @ignore */
+private static deserializeLocationError(
+locationError: LocationErrorInternal
+): LocationError {
+switch (locationError.type) {
+case 'notRunning':
+case 'starting':
+return locationError;
+case 'errors':
+return {
+type: 'errors',
+value: this.deserializeHyperTrackErrors(locationError.value),
+};
+}
+}
+
+/** @ignore */
+private static deserializeLocationResponse(
+response: Result<LocationInternal, LocationErrorInternal>
+): Result<Location, LocationError> {
+switch (response.type) {
+case 'success':
+return {
+type: 'success',
+value: response.value.value,
+};
+case 'failure':
+return {
+type: 'failure',
+value: this.deserializeLocationError(response.value),
+};
+}
+}
+
+/** @ignore */
+private static deserializeLocationWithDeviationResponse(
+response: Result<LocationWithDeviationInternal, LocationErrorInternal>
+): Result<LocationWithDeviation, LocationError> {
+switch (response.type) {
+case 'success':
+const locationWithDeviationInternal: LocationWithDeviationInternal =
+response.value;
+const locationInternal: LocationInternal =
+locationWithDeviationInternal.value.location;
+
+return {
+type: 'success',
+value: {
+location: locationInternal.value,
+deviation: locationWithDeviationInternal.value.deviation,
+} as LocationWithDeviation,
+};
+case 'failure':
+return {
+type: 'failure',
+value: this.deserializeLocationError(response.value),
+};
+}
+}
+
+/** @ignore */
+private static deserializeMetadata(metadata: Metadata): Object {
+if (metadata.type != 'metadata') {
+throw new Error(`Invalid metadata: ${JSON.stringify(metadata)}`);
+}
+return metadata.value;
+}
+
+/** @ignore */
+private static deserializeName(name: Name): string {
+if (name.type != 'name') {
+throw new Error(`Invalid name: ${JSON.stringify(name)}`);
+}
+return name.value;
+}
+
+/** @ignore */
+private static isLocation(obj: Location): obj is Location {
+return (
+'latitude' in obj &&
+typeof obj.latitude == 'number' &&
+'longitude' in obj &&
+typeof obj.longitude == 'number'
+);
+}
+}
+*/
 
 import 'package:flutter/services.dart';
 import 'package:hypertrack_plugin/data_types/json.dart';
@@ -14,24 +479,32 @@ import 'data_types/location.dart';
 
 /// This plugin allows you to use HyperTrack SDK for Flutter apps
 class HyperTrack {
+  /// Adds a new geotag.
+  /// Accepts [data] - Geotag data JSON.
+  /// Returns current location if success or [LocationError] if failure.
   static Future<Result<Location, LocationError>> addGeotag(JSONObject data) {
     return _invokeSdkMethod<Map<Object?, Object?>>(
-            SdkMethod.addGeotag, serializeGeotagData(data, null))
+        SdkMethod.addGeotag, serializeGeotagData(data, null))
         .then((value) {
       return deserializeLocationResult(value);
     });
   }
 
+  /// Adds a new geotag with expected location.
+  /// Accepts [data] - Geotag data JSON.
+  /// Accepts [expectedLocation] - Expected location.
+  /// Returns current location with deviation from expected location if success
+  /// or [LocationError] if failure.
   static Future<Result<LocationWithDeviation, LocationError>>
-      addGeotagWithExpectedLocation(
-          JSONObject data, Location expectedLocation) {
+  addGeotagWithExpectedLocation(JSONObject data, Location expectedLocation) {
     return _invokeSdkMethod<Map<Object?, Object?>>(
-            SdkMethod.addGeotag, serializeGeotagData(data, expectedLocation))
+        SdkMethod.addGeotag, serializeGeotagData(data, expectedLocation))
         .then((value) {
       return deserializeLocationWithDeviationResult(value);
     });
   }
 
+  /// Returns a string that is used to uniquely identify the device.
   static Future<String> get deviceId async {
     return _invokeSdkMethod<Map<Object?, Object?>>(SdkMethod.getDeviceID)
         .then((value) {
@@ -39,12 +512,14 @@ class HyperTrack {
     });
   }
 
+  /// Returns a list of errors that blocks the SDK from tracking.
   static Future<Set<HyperTrackError>> get errors async {
     return _invokeSdkMethod<List<Object?>>(SdkMethod.getErrors).then((value) {
       return deserializeErrors(value.cast<Map<Object?, Object?>>());
     });
   }
 
+  /// Reflects availability of the device for the Nearby search.
   static Future<bool> get isAvailable async {
     return _invokeSdkMethod<Map<Object?, Object?>>(SdkMethod.getIsAvailable)
         .then((value) {
@@ -52,6 +527,7 @@ class HyperTrack {
     });
   }
 
+  /// Reflects the tracking intent for the device.
   static Future<bool> get isTracking async {
     return _invokeSdkMethod<Map<Object?, Object?>>(SdkMethod.getIsTracking)
         .then((value) {
@@ -59,6 +535,8 @@ class HyperTrack {
     });
   }
 
+
+  /// Reflects the current location of the user or an outage reason.
   static Future<Result<Location, LocationError>> get location async {
     return _invokeSdkMethod<Map<Object?, Object?>>(SdkMethod.getLocation)
         .then((value) {
@@ -66,6 +544,7 @@ class HyperTrack {
     });
   }
 
+  /// Gets the metadata that is set for the device.
   static Future<JSONObject> get metadata async {
     return _invokeSdkMethod<Map<Object?, Object?>>(SdkMethod.getMetadata)
         .then((value) {
@@ -73,6 +552,7 @@ class HyperTrack {
     });
   }
 
+  /// Gets the name that is set for the device.
   static Future<String> get name async {
     return _invokeSdkMethod<Map<Object?, Object?>>(SdkMethod.getName)
         .then((value) {
@@ -80,48 +560,58 @@ class HyperTrack {
     });
   }
 
+  /// Requests one-time location update and returns the [Location] once it is
+  /// available, or [LocationError].
   static Stream<Result<Location, Set<HyperTrackError>>> locate() {
     return _locateChannel.receiveBroadcastStream().map((event) {
       return deserializeLocateResult(event);
     });
   }
 
+  /// Sets the availability of the device for the Nearby search.
   static void setIsAvailable(bool available) {
     _invokeSdkVoidMethod(
         SdkMethod.setIsAvailable, serializeIsAvailable(available));
   }
 
+  /// Sets the tracking intent for the device.
   static void setIsTracking(bool tracking) {
     _invokeSdkVoidMethod(
         SdkMethod.setIsTracking, serializeIsTracking(tracking));
   }
 
-  static void setName(String name) {
-    _invokeSdkVoidMethod(SdkMethod.setName, serializeName(name));
-  }
-
+  /// Sets the metadata for the device.
   static void setMetadata(JSONObject data) {
     _invokeSdkVoidMethod(SdkMethod.setMetadata, serializeMetadata(data));
   }
 
+  /// Sets the name for the device.
+  static void setName(String name) {
+    _invokeSdkVoidMethod(SdkMethod.setName, serializeName(name));
+  }
+
+  /// Subscribe to tracking errors.
   static Stream<Set<HyperTrackError>> get errorsSubscription {
     return _errorsChannel.receiveBroadcastStream().map((event) {
       return deserializeErrors(event.cast<Map<Object?, Object?>>());
     });
   }
 
+  /// Subscribe to availability changes.
   static Stream<bool> get isAvailableSubscription {
     return _isAvailableChannel.receiveBroadcastStream().map((event) {
       return deserializeIsAvailable(event);
     });
   }
 
+  /// Subscribe to tracking intent changes.
   static Stream<bool> get isTrackingSubscription {
     return _isTrackingChannel.receiveBroadcastStream().map((event) {
       return deserializeIsTracking(event);
     });
   }
 
+  /// Subscribe to location changes.
   static Stream<Result<Location, LocationError>> get locationSubscription {
     return _locationChannel.receiveBroadcastStream().map((event) {
       return deserializeLocationResult(event);
@@ -167,9 +657,9 @@ const _methodChannel = MethodChannel('$_channelPrefix/methods');
 // channels for receiving events from the SDK
 const EventChannel _errorsChannel = EventChannel('$_channelPrefix/errors');
 const EventChannel _isAvailableChannel =
-    EventChannel('$_channelPrefix/isAvailable');
+EventChannel('$_channelPrefix/isAvailable');
 const EventChannel _isTrackingChannel =
-    EventChannel('$_channelPrefix/isTracking');
+EventChannel('$_channelPrefix/isTracking');
 const EventChannel _locationChannel = EventChannel('$_channelPrefix/location');
 const EventChannel _locateChannel = EventChannel('$_channelPrefix/locate');
 
